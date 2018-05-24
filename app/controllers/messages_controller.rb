37e112b9885342c33c4b1f1d2daeb2d07d464b5a
shukan0728 <cielo28verano@gmail.com>
2018-05-24 19:49:12 +0900

    Edit messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,14 +8,18 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message.save
+    @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to group_messages_path(@group), notice: 'メッセージが送信されませんでした'
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
       render :index
     end
+  end
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 
 
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,7 +3,7 @@ class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
 
-   mount_uploader :image, ImageUploader
+  mount_uploader :image, ImageUploader
 
   validates :content, presence: true, unless: :image?
 
