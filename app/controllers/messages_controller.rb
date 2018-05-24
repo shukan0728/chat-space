ded10329cbc552922599e89489c5ea254bdc4b64
shukan0728 <cielo28verano@gmail.com>
2018-05-24 15:20:45 +0900

    Edit around messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,26 @@
 class MessagesController < ApplicationController
 
+  before_action :set_group
 
-def index
-end
+  def index
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
+  end
+
+  def create
+    @message.save
+    if @message.save
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されませんでした'
+    else
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :index
+    end
+  end
+
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
 
 end
