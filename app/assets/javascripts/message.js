f5a2c2b5baf0730878057e1668760adec9149261
shukan0728 <cielo28verano@gmail.com>
2018-05-28 19:52:17 +0900

    メッセージ機能の非同期化

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,53 @@
+$(function(){
+  function buildHTML(message){
+    var img = "";
+    if (message.image) {
+      img= `<img src= ${message.image} class="lower__message__image">`;
+    }
+    var html =
+            `<div class="chat-contents__content">
+          <div class="chat-contents__content__upper">
+            <div class="chat-contents__content__upper__name">
+              ${message.user_name}
+            </div>
+            <div class="chat-contents__content__upper__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="chat-contents__content__lower">
+              <p class="chat-contents__content__lower__message">
+                ${message.content}</p>
+            ${img}
+          </div>
+          </div>`;
+          return html;
+    }
+
+  $(function(){
+    $('#new_message').on('submit', function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action');
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.chat-contents').append(html);
+        console.log(html)
+        $('.form__message').val("");
+        $('.form__mask__image').val("");
+        $('.form__submit').prop('disabled', false);
+        $('.chat-contents').animate({scrollTop: $('.chat-contents')[0].scrollHeight});
+      })
+      .fail(function(){
+        alert(`error`);
+      })
+    });
+  });
+});
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,5 @@
+json.id @message.id
+json.user_name @message.user.name
+json.content @message.content
+json.created_at @message.created_at
+json.image @message.image.url
