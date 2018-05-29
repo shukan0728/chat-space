69084a8bf3b33eec5d6d5ce21e936b090de4101d
shukan0728 <cielo28verano@gmail.com>
2018-05-29 20:26:53 +0900

    チャットメンバーの追加と削除

--- app/assets/javascripts/user_inc.js
+++ app/assets/javascripts/user_inc.js
@@ -15,6 +15,15 @@ $(function() {
     inc_list.append(html);
   };
 
+  function buildHTML(id, name) {
+    var html = `<div class="chat-group-user clearfix" id=chat-group-user-${id}>
+                  <input type="hidden" name="user_id" value="${id}">
+                  <p class="chat-group-user__name">${name}</p>
+                  <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove" data-user-id="${id}">削除</a>
+                </div>`
+    return html
+  }
+
   $(function() {
     $("#user-search-field").on("keyup", function() {
       var input = $("#user-search-field").val();
@@ -29,7 +38,6 @@ $(function() {
         if (users.length !== 0) {
          users.forEach(function(user){
             appendUser(user)
-            console.log(user)
          });
         } else {
           appendNoUser("一致するユーザーはいません")
@@ -40,5 +48,19 @@ $(function() {
       })
     });
   });
+
+    $(document).on('click','.user-search-add', function() {
+    var id = $(this).data('user-id');
+    var name = $(this).data('user-name');
+    var insertHTML = buildHTML(id, name);
+    $('.chat-group-users').append(insertHTML);
+    $(this).parent('.chat-group-user').remove();
+  })
+
+  $(document).on('click', '.user-search-remove', function() {
+    var id = $(this).data('user-id');
+    $(`#chat-group-user-${id}`).remove();
+  })
+
 });
 
