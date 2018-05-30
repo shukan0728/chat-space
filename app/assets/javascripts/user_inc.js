05d553bbb40ae8229cc5375f992782a76c1ade97
shukan0728 <cielo28verano@gmail.com>
2018-05-30 15:22:03 +0900

    #3修正/インクリメンタルサーチの実装

--- app/assets/javascripts/user_inc.js
+++ app/assets/javascripts/user_inc.js
@@ -4,8 +4,8 @@ $(function() {
 
   function appendUser(user) {
     var html = `<div class='chat-group-user clearfix' id='chat-group-user-${user.id}'>
-                    <p class='chat-group-user__name'>${user.name}</p>
-                      <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                  <p class='chat-group-user__name'>${user.name}</p>
+                    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
                 </div>`
     inc_list.append(html)
   };
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,22 +7,22 @@ class UsersController < ApplicationController
   end
 
   def update
-  if current_user.update(user_params)
-     redirect_to root_path
-  else
-     render :edit
-  end
- end
-
- def index
-  @users = User.where.not(id: current_user.id).where('name LIKE(?)', "%#{params[:name]}%")
-  respond_to do |format|
-    format.html
-    format.json
-  end
+    if current_user.update(user_params)
+       redirect_to root_path
+    else
+       render :edit
+    end
   end
 
- private
+  def index
+    @users = User.where.not(id: current_user.id).where('name LIKE(?)', "%#{params[:name]}%")
+    respond_to do |format|
+      format.html
+      format.json
+  end
+  end
+
+  private
 
   def user_params
     params.require(:user).permit(:name, :email)
