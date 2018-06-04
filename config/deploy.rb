e03ce847115cebdf59be6dda8fcace93759e37b1
shukan0728 <cielo28verano@gmail.com>
2018-06-04 21:58:10 +0900

    Version change

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,7 +40,7 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
-lock '~> 3.11.0'
+lock '3.11.0'
 
 set :application, 'chat-space'
 set :repo_url,  'git@github.com:shukan0728/chat-space.git'
